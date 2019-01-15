966e02d55dd6ac92e3375fb4e7d3aa7faa2933f7
tomaru <sta.100100@gmail.com>
2019-01-15 23:00:51 +0900

    csv出力操作追加

--- csv_read.py
+++ csv_read.py
@@ -43,15 +43,24 @@ test["Embarked"][ test["Embarked"] == "Q" ] = 2
 #print( train.head(10) )
 #print( test.head(10) )
 
+# 「train」の目的変数と説明変数の値を取得
 target = train["Survived"].values
 features_one = train[ ["Pclass", "Sex", "Age", "Fare"] ].values
 print(features_one)
-
+# 決定木の作成
 my_tree_one = tree.DecisionTreeClassifier()
 my_tree_one.fit( features_one, target )
-
+# 「test」の説明変数の値を取得
 test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+# 「test」の説明変数を使って「my_tree_one」のモデルで予測
 my_prediction = my_tree_one.predict(test_features)
 print(f"予想結果:{my_prediction}")
+
+# PassengerIdを取得
+PassengerId = np.array( test["PassengerId"].astype(int) )
+# my_prediction(予測データ）とPassengerIdをデータフレームへ落とし込む
+my_solution = pd.DataFrame( my_prediction, PassengerId, columns = ["Suvived"] )
+# my_tree_one.csvとして書き出し
+my_solution.to_csv( "my_tree_one.csv", index_label = ["PassengerId"] )
 
 
